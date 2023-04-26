ce4c56531f100d7b779495ac7f3cad7c333411bc
DanielFitzsimons <80958065+DanielFitzsimons@users.noreply.github.com>
2023-04-26 12:35:31 +0100

    added to server

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
 var bodyParser = require('body-parser')
+const bcrypt = require("bcrypt")
+const handleSignUp = require('./handlers/signup');
 const cors = require('cors');
 
 app.use(bodyParser.json())
@@ -14,19 +16,37 @@ app.use(cors());
 //mongoose database
 main().catch(err => console.log(err));
 async function main(){
-    await  await mongoose.connect('mongodb+srv://football1:football1@cluster0.6kgyhyt.mongodb.net/?retryWrites=true&w=majority');
+    await  await mongoose.connect('mongodb+srv://root1:user1@cluster0.8kcocq3.mongodb.net/test');
     //use `await mongoose.connect('mongodb://username:password@localhost:27017/test');`
 }
 
-const teamSchema = new mongoose.Schema({
-    teamLogo: String,
-    teamName: String,
-    founded: String, 
-    trophies: String,
-    rivals: String,
-});
+const userSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  });
 
-const teamModel = mongoose.model('Teams', teamSchema);
+const userModel = mongoose.model('User', userSchema);
+
+app.post('/api/register', handleSignUp);
+
+app.get('/login', (req, res) =>{
+    res.render("components/login.js")
+})
+
+app.get('/api/register', (req, res) =>{
+    res.render("components/register.js")
+})
 
 app.listen(port, ()=> {
     console.log(`App listening on port ${port}`)
