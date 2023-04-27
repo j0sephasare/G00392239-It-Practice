359139cf9ff1a5998e20f082075dbe7dfaf88c77
DanielFitzsimons <80958065+DanielFitzsimons@users.noreply.github.com>
2023-04-27 22:30:42 +0100

    add homePage

---
+++ src/components/homePage.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import '../App.css'
+import Navbar1 from './Navbar'
+import axios from 'axios'
+
+
+const HomePage = () => {
+
+    
+
+  return (
+    <div>
+      <Navbar1 />
+       
+    </div>
+  );
+}
+
+export default HomePage
