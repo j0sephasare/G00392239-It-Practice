d6c3eb22b892b94340e437f680fee9b3adb1a67a
Joseph <1josephasare1@gmail.com>
2023-04-28 09:47:16 +0100

    new commit

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,17 +1,37 @@
-import React, { useState } from 'react'
-import '../App.css'
-import Navbar1 from './Navbar'
-import axios from 'axios'
-
+import React, { useState } from 'react';
+import '../App.css';
+import Navbar1 from './Navbar';
+import axios from 'axios';
 
 const HomePage = () => {
+return (
+<div>
+<Navbar1 />
+<div style={{ textAlign: 'center' }}>
+<img src="https://i2-prod.football.london/incoming/article26782992.ece/ALTERNATES/s1200/0_Tottenham-vs-MU.png" width="500" center="ce" alt="Image description" />
+</div>
+<div style={{ textAlign: 'center' }}>
+<p>Hello and welcome to live coverage of Tottenham's Premier League clash against Manchester United.</p>
+<p>Following Spurs' 6-1 defeat against Newcastle United on Sunday afternoon, it is a huge game for the Lilywhites as they look to bounce back and keep themselves within touching distance of the top four.</p>
+<p>It is also Ryan Mason's first game back at the helm after he replaced Cristian Stellini in the hotseat on Monday evening following the Italian's dismissal as acting head coach after four matches.</p>
+<p>Manchester United come into the game on the back of their FA Cup semi-final victory over Brighton & Hove Albion, although they have struggled on the road against a number of the teams in the top half of the Premier League this season.</p>
+<p>Tottenham correspondent Rob Guest are covering the match with all the build-up and the action as it happens on the pitch and reaction from the supporters and media.</p>
+<p>Scroll down for all of our latest updates from Tottenham Hotspur Stadium.</p>
+</div>
+<div style={{ textAlign: 'center' }}>
+<img src="https://e0.365dm.com/23/04/1600x900/skysports-man-city-arsenal_6134787.jpg?20230427071246" width="500" center="ce" alt="Image description" />
+</div>
+<div style={{ textAlign: 'center' }}>
+<p>Mikel Arteta looked crestfallen afterwards, bereft that the level of his team had appeared to dip in what was the biggest game of the Premier League season. Pep Guardiola was probably closer to the truth in his assessment that they had forced that dip.</p>
+<p>Up against it from the outset, Arsenal could not play through Man City's press nor prevent them from passing out. When Ederson looked longer, finding Kevin De Bruyne in space or Erling Haaland up against defenders he could bully, the problems deepened.</p>
+<p>The two men combined for three of the four goals, decisive when it mattered, but the difference between the teams extended beyond two players. There was a gulf and if there is a gulf against Arsenal it is hard to imagine City slipping twice in the seven games remaining.</p>
+<p>Guardiola was as giddy as he gets afterwards, content that he had got the big decisions right. The combative Manuel Akanji at left-back was a key move. He admitted that Aymeric Laporte was the more natural pick. But the battle with Bukayo Saka was won.</p>
+<p>Rodri controlled the midfield, Jack Grealish has been transformed into a tigerish wing warrior, doing his defensive work with diligence and enthusiasm. Arsenal left the door ajar and City have burst through it. When they are this good, they are too good for anyone.</p>
+<p>If Arsenal's title race has been run
+        </p>
+        </div>
 
-    
-
-  return (
-    <div>
-      <Navbar1 />
-       
+     
     </div>
   );
 }
